868efc918dee614c7909cf8299685d4220a2560f
lenovo <lenovo@DESKTOP-31C3901>
2018-06-20 10:37:21 +0530

    done it

--- EmployeManagement/src/main/java/com/java/Employe/Dao/EmployeeMang.java
+++
@@ -1,9 +0,0 @@
-package com.java.Employe.Dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.java.Employe.entity.Employee;
-
-public interface EmployeeMang extends CrudRepository<Employee, Integer> {
-
-}
--- VechicalService/src/main/java/com/java/vehical/vechicleDao/Vechicledao.java
+++
@@ -1,9 +0,0 @@
-package com.java.vehical.vechicleDao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.java.vehical.entity.Vechicle;
-
-public interface Vechicledao extends CrudRepository<Vechicle, Integer> {
-
-}
